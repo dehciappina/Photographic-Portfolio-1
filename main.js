9d84029a0eba83ab08bee3cec7f76fc6701fb5dd
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-05-09 17:36:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 
+//QUOTE TRANSITION
+
+$(document).ready(function() {
+
+    //sequence of 0 to 2
+    numberSequence = 0
+
+    //shows up the first quote when page loads
+    $('.main_quote div:eq('+numberSequence+')').stop().fadeIn(3000)
+
+    //the function that makes things change
+    function showNextQuote() {
+        
+        //fade out the next value and call the below function
+        $('.main_quote div:eq('+numberSequence++ +')').stop().fadeOut(1500).queue(function() {
+
+            //fade in the updated numberSequence value
+            $('.main_quote div:eq('+numberSequence+')').fadeIn(2000).dequeue()
+        })
+      
+      console.log(numberSequence)
+      
+      //if it gets bigger than 2, there is no quotes left to show. So its necessary to reset the value.
+      if(numberSequence > 2) {
+        numberSequence = 0
+      }
+    }
+
+    //it calls the function to make it happen, within an defined interval.
+    setInterval(function() {
+      showNextQuote() 
+    }, 5000)
+})
+
 
 if($(window).width() > 670) {
 $('.contact_button').click(function() {
@@ -61,16 +95,18 @@ $('.imgs_container').click(function() {
         scrollTop: $(this).offset().top - 53 + '%',
     }, 400);
 
-    $('.leave_gallery').toggle()
-})
+    $('.leave_gallery').toggle().queue(function() {
+       $('.see_more').slideToggle(600)
+    }).dequeue()
 
-$('.imgs_container').click(function() {
-    $(this).find('.hidden_gallery').finish().fadeToggle(400, 'easeOutCubic')
+    $(this).find('.hidden_gallery').finish().fadeToggle(300, 'easeOutCubic')
     $(this).siblings().finish().toggleClass('low_brightness')
     $(this).siblings().find('.hidden_gallery').finish().slideUp(800, 'easeOutCubic')
     $('.image_container').finish().toggleClass('low_brightness')
-})
 
+    $('.footer').toggle('blind', { direction: "down" }, toggleBars);
+    $('.top_bar').toggle('blind', toggleBars);
+})
 
 $('.leave_gallery').click(function() {
     $(this).hide()
@@ -78,4 +114,9 @@ $('.leave_gallery').click(function() {
     $('.imgs_container').find('.main_img_selected').finish().show()
     $('.imgs_container').siblings().finish().removeClass('low_brightness')
     $('.image_container').finish().removeClass('low_brightness')
+    $('.top_bar').toggle('blind', toggleBars);
+    $('.footer').toggle('blind', { direction: "down" }, toggleBars);
+    $('.see_more').slideToggle(300)
 })
+
+toggleBars = 250
